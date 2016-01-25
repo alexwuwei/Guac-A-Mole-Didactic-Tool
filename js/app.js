7092efa9f2c3354ed19042b6f32ca2a8c3e30b2f
Rubiksron <rubiksron@gmail.com>
2016-01-25 14:45:08 -0800

    started constructor

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function QuestionAnswer(questionString, rightAnswer, wrongAnswerOne, wrongAnswerTwo, wrongAnswerThree, category) {
+  this.questionString = questionString;
+  this.rightAnswer = rightAnswer;
+  this.wrongAnswerOne = wrongAnswerOne;
+  this.wrongAnswerTwo = wrongAnswerTwo;
+  this.wrongAnswerThree = wrongAnswerThree;
+}
